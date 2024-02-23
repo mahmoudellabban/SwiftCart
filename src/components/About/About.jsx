3a2eac03b8b1bd432623e527f8a59d06e3439819
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-02-23 21:20:19 +0200

    commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { FaExternalLinkAlt } from "react-icons/fa";
 const About = () => {
   return (
-    <div className="container">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorem
-      autem reprehenderit dignissimos pariatur rerum.
-    </div>
+    <section className="container">
+      <h5>
+        SwiftCart is an E-commerce demo website designed and developed by 
+        <span>
+          <a
+            href="https://mahmoudellabban.github.io/myWebsite/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaExternalLinkAlt /> Mahmoud Fathy Ellabban 
+          </a>
+        </span> 
+        who has a Bachelor's degree in Electronics and Communication Engineering
+        and currently works as a Front-End Developer
+      </h5>
+    </section>
   );
 };
 
