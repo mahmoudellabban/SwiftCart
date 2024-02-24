e23ea9976c7058698d0bab56818560d8fda8267f
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-02-24 19:18:32 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import "./App.css"
-import Navbar from "./components/Navbar/Navbar"
-import Home from './components/Home/Home'
-import Products from "./components/Products/Products"
-import Product from './components/Product/Product'
-import About from './components/About/About'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Product from "./components/Product/Product";
+import About from "./components/About/About";
 const App = () => {
-  
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="products" element={<Products />}/>
-        <Route path="products/:id" element={<Product/>}/>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<Product />} />
         <Route path="about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { TbWorld } from "react-icons/tb";
 import "./about.css";
-import { FaExternalLinkAlt, FaLongArrowAltRight } from "react-icons/fa";
+import {FaInstagram, FaFacebook, FaLinkedin, FaGithub, FaExternalLinkAlt, FaLongArrowAltRight } from "react-icons/fa";
 const About = () => {
   function seeWebsite() {
     const websiteUrl = "https://mahmoudellabban.github.io/myWebsite/";
@@ -16,18 +17,45 @@ const About = () => {
             <FaExternalLinkAlt /> Mahmoud Fathy Ellabban
           </span>{" "}
           who has a Bachelor's degree in Electronics and Communication
-          Engineering and currently works as a Front End Developer.
+          Engineering and currently works as a Front End Developer. Down below
+          you will find my github account link, feel free to use anything from
+          this project's repository, or any other Repositories, they're public.
         </div>
         <div className="second">
           <h2>technologies used</h2>
-          <p>React <FaLongArrowAltRight /> JavaScript Library</p>
-          <p>Redux <FaLongArrowAltRight /> State Management</p>
-          <p>Clerk <FaLongArrowAltRight /> Authentication</p>
-          <p>CSS <FaLongArrowAltRight /> Styling</p>
-          <p>Material UI <FaLongArrowAltRight /> Styling</p>
-          <p>React Skeleton <FaLongArrowAltRight /> Loading Skeleton</p>
-          <p>React Icons <FaLongArrowAltRight /> Icons</p>
-          <p>React Router Dom <FaLongArrowAltRight /> Routing</p>
+          <p>
+            React <FaLongArrowAltRight /> JavaScript Library
+          </p>
+          <p>
+            Redux <FaLongArrowAltRight /> State Management
+          </p>
+          <p>
+            React Skeleton <FaLongArrowAltRight /> Loading Skeleton
+          </p>
+          <p>
+            React Router Dom <FaLongArrowAltRight /> Routing
+          </p>
+          <p>
+            React Icons <FaLongArrowAltRight /> Icons
+          </p>
+          <p>
+            Clerk <FaLongArrowAltRight /> Authentication
+          </p>
+          <p>
+            CSS <FaLongArrowAltRight /> Styling
+          </p>
+          <p>
+            Material UI <FaLongArrowAltRight /> Styling
+          </p>
+          
+        </div>
+        <div className="second">
+          <h2>Contact me</h2>
+          <a href="https://github.com/mahmoudellabban" rel="noreferrer" target="_blank"> <FaGithub /> Github</a>
+          <a href="https://mahmoudellabban.github.io/myWebsite/" rel="noreferrer" target="_blank"> <TbWorld /> My Website</a>
+          <a href="https://www.linkedin.com/in/mahmoud-ellabban/" rel="noreferrer" target="_blank"> <FaLinkedin /> LinkedIn</a>
+          <a href="https://www.facebook.com/mahmoud.ellabban.7" rel="noreferrer" target="_blank"> <FaFacebook /> Facebook</a>
+          <a href="https://www.instagram.com/mahmoud.ellabban.74/" rel="noreferrer" target="_blank"> <FaInstagram /> Instagram</a>
         </div>
       </div>
     </section>
