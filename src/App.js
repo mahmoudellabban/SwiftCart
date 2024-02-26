2975e51d23e0880650d9d33bf695680ec1499f0a
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-02-26 18:42:02 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/Home/Home"
 import Products from "./components/Products/Products";
 import Product from "./components/Product/Product";
 import About from "./components/About/About";
